2f6681bc34360d2a296b5e1ce0dd6f1356f2785d
Christoph Daecke <typo3@mediadreams.org>
2020-10-29 16:44:45 +0100

    [TASK] remove obsolete code

--- Classes/Utility/PreviewUrlUtility.php
+++ Classes/Utility/PreviewUrlUtility.php
@@ -13,8 +13,6 @@ namespace ChristianEssl\Impersonate\Utility;
  ***/
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 /**
  * Get the preview url
@@ -23,65 +21,16 @@ class PreviewUrlUtility
 {
     public static function getPreviewUrl($pageId)
     {
-        // the cool way
-        if (VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9000000) {
-            $switchFocus = true;
-            return BackendUtility::getPreviewUrl(
-                $pageId,
-                '',
-                null,
-                '',
-                '',
-                '',
-                $switchFocus
-            );
-        } else {
-            // the old way
-            $protocolAndHost = self::getProtocolAndHost($pageId);
-            return $protocolAndHost . '/index.php?id=' . $pageId;
-        }
-    }
-
-    /**
-     * The old preview url method from ViewModuleController in TYPO3 8.7
-     *
-     * @param integer $pageId
-     *
-     * @return string
-     */
-    protected static function getProtocolAndHost($pageId)
-    {
-        $protocolAndHost = '..';
-        $domainName = self::getDomainName($pageId);
-        if ($domainName) {
-            // TCEMAIN.previewDomain can contain the protocol, check prevents double protocol URLs
-            if (strpos($domainName, '://') !== false) {
-                $protocolAndHost = $domainName;
-            } else {
-                $protocol = GeneralUtility::getIndpEnv('TYPO3_SSL') ? 'https' : 'http';
-                $protocolAndHost = $protocol . '://' . $domainName;
-            }
-        }
-        return $protocolAndHost;
-    }
-
-    /**
-     * Get domain name for requested page id
-     * Taken from ViewModuleController in TYPO3 8.7
-     *
-     * @param int $pageId
-     * @return string|null Domain name from first sys_domains-Record or from TCEMAIN.previewDomain, NULL if neither is configured
-     */
-    protected static function getDomainName($pageId)
-    {
-        $previewDomainConfig = $GLOBALS['BE_USER']
-            ->getTSConfig('TCEMAIN.previewDomain', BackendUtility::getPagesTSconfig($pageId));
-        if ($previewDomainConfig['value']) {
-            $domain = $previewDomainConfig['value'];
-        } else {
-            $domain = BackendUtility::firstDomainRecord(BackendUtility::BEgetRootLine($pageId));
-        }
-        return $domain;
+        $switchFocus = true;
+        return BackendUtility::getPreviewUrl(
+            $pageId,
+            '',
+            null,
+            '',
+            '',
+            '',
+            $switchFocus
+        );
     }
 
 }
